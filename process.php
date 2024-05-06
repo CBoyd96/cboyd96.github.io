63f27f608291a2fe143d22f95d545fa895bc2688
Cameron Boyd <52261110+CBoyd96@users.noreply.github.com>
2024-05-05 22:45:44 -0400

    Update process.php

--- process.php
+++ process.php
@@ -1,1 +1,7 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST["username"]);
+    echo "Hello, " . $username . "! Your name has been added to the database.";
+}
+?>
 
